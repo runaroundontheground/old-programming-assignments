22930590d14876718902e15abb5153aaef21cfd7
runaroundontheground <118295064+runaroundontheground@users.noreply.github.com>
2022-11-17 10:00:12 -0700

    Add files via upload

---
+++ mad lib.py
@@ -0,0 +1,22 @@
+#this is supposed to be a mad lib
+#have fun
+#all the inputs:
+color = input("Type a color: ")
+superlative = input("Type a superlative (a word that ends with 'est', like 'bravest'): ")
+adjective1 = input("Type an adjective (word that describes something, ex. shiny): ")
+bodyPartPlural = input("Type a body part (plural): ")
+bodyPart = input("Type a body part (not plural: ")
+noun = input("Type a noun (person place or thing): ")
+animalPlural = input("Type an animal (plural): ")
+adjective2 = input("Type an adjective: ")
+adjective3 = input("Type an adjective: ")
+adjective4 = input("Type an adjective: ")
+
+#actual story:
+print("The " + color + " Dragon is the " + superlative
+ + " of all. It is also " + color + ". It has " + adjective1 +
+ " " + bodyPartPlural + ", and a " + bodyPart + " shaped like a " 
+ + noun + ". The " + color + " Dragon is also the " + superlative
+  + " of all. It loves to eat " + animalPlural + ", although it will feast "
+  + "on nearly anything. It is " + adjective2 + " and " + adjective3
+  + ". You must be " + adjective4 + " around it, or you may end up as it's meal!")
